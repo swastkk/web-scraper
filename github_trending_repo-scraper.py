97e72eebd0be11c2b70be82b207e78a5d1d2cdd4
swastik <swastkk@gmail.com>
2023-01-25 00:51:26 +0530

    Repo name and link done but for 1 only :)
    Signed-off-by: swastik <swastkk@gmail.com>

--- github_trending_repo-scraper.py
+++ github_trending_repo-scraper.py
@@ -9,11 +9,6 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 # get the repo list
 repos = soup.find(class_="Box-row")
-repo_div= repos.find('div', class_='f6 color-fg-muted mt-2')
-star_a= repo_div.find('a', class_='Link--muted d-inline-block mr-3')
-# find all instances of that class (should return 25 as shown in the github main page)
-# repo_list = repo.find_all(class_='col-12 d-block width-full py-4 border-bottom')
-
 print(len(repos))
 
 # Open writer with name
@@ -24,20 +19,23 @@ f = csv.writer(open(file_name, 'w', newline=''))
 # write a new row as a header
 f.writerow(['Developer', 'Repo Name', 'Number of Stars'])
 
-for repo in repos:
-    # find the first <a> tag and get the text. Split the text using '/' to get an array with developer name and repo name
-    
-    # full_repo_name = repo_head.find('a['href']').text.split('/')
-    # extract the developer name at index 0
-   # developer = full_repo_name[0].strip()
-    # extract the repo name at index 1
-    # repo_name = full_repo_name[1].strip()
-    # find the first occurance of class octicon octicon-star and get the text from the parent (which is the number of stars)
+for repo in repos():
+    #------For the repo name and owner--------
+    repo_header= repos.find('h1')
+    repo_a= repo_header.find("a", href=True)
+    #-----------------------------------------
+    full_repo_name= repo_a["href"]
+    # For the stars------------
+    repo_div= repos.find('div', class_='f6 color-fg-muted mt-2')
+    star_a= repo_div.find('a', class_='Link--muted d-inline-block mr-3')
+    #--------------------------
     stars = star_a.find('svg', class_='octicon octicon-star').parent.text.strip()
     # strip() all to remove leading and traling white spaces
-  #  print('developer', developer)
-   # print('name', repo_name)
-    print('stars', stars)
-    print('Writing rows')
-    # add the information as a row into the csv table
-    f.writerow([developer, repo_name, stars])
+    # print('developer', developer)
+    # print('name', repo_name)
+
+
+print(full_repo_name)
+print('stars', stars)
+      # add the information as a row into the csv table
+f.writerow([full_repo_name, stars])
