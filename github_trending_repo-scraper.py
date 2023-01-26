8eb47095e1bce591c3208d5f97a24cb56ce46017
swastik <swastkk@gmail.com>
2023-01-26 14:56:41 +0530

    Errors fixed
    Signed-off-by: swastik <swastkk@gmail.com>

--- github_trending_repo-scraper.py
+++ github_trending_repo-scraper.py
@@ -8,8 +8,10 @@ page = requests.get('https://github.com/trending')
 soup = BeautifulSoup(page.text, 'html.parser')
 
 # get the repo list
-repos = soup.find(class_="Box-row")
-print(len(repos))
+repo_page= soup.find('div', class_='Box')
+repos= repo_page.find('div', attrs={'class': None})  #error is due to this line only..... please fix this :)
+repo_list = repos.find_all('article',class_="Box-row")
+print(len(repo_list))
 
 # Open writer with name
 file_name = "github_trending_today.csv"
@@ -17,16 +19,16 @@ file_name = "github_trending_today.csv"
 f = csv.writer(open(file_name, 'w', newline=''))
 
 # write a new row as a header
-f.writerow(['Developer', 'Repo Name', 'Number of Stars'])
+f.writerow(['Repo Name', 'Number of Stars'])
 
-for repo in repos():
+for repo in repo_list:
     #------For the repo name and owner--------
-    repo_header= repos.find('h1')
+    repo_header= repo_list.find('h1')
     repo_a= repo_header.find("a", href=True)
     #-----------------------------------------
     full_repo_name= repo_a["href"]
     # For the stars------------
-    repo_div= repos.find('div', class_='f6 color-fg-muted mt-2')
+    repo_div= repo_list.find('div', class_='f6 color-fg-muted mt-2')
     star_a= repo_div.find('a', class_='Link--muted d-inline-block mr-3')
     #--------------------------
     stars = star_a.find('svg', class_='octicon octicon-star').parent.text.strip()
